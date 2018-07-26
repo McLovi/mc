4ac414135979e516fd51d0b95f379e8ed915e480
JBIsaik <41596157+JBIsaik@users.noreply.github.com>
2018-07-26 13:02:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ lines = open(r'usermcalts.txt').read().splitlines()
 @bot.event
 async def on_ready():
     print('The bot is online!')
+    await bot.change_presence(game=discord.Game(name='Type <spotify'))
    
     
 @bot.command(pass_context=True)
@@ -20,13 +21,14 @@ async def minecraft(ctx):
     if ctx.message.server:
         myline = random.choice(lines)
         split = myline.partition(":")
-        
-        embed=discord.Embed(title="Minecraft Account", color=0xf45eff)
+       
+        embed=discord.Embed(title="Spotify Account", color=0xf45eff)
         embed.set_thumbnail(url="https://pre00.deviantart.net/3d13/th/pre/i/2016/343/0/7/free_minecraft_logo_template_by_curtzeninja-dar2dou.png")
         embed.add_field(name="Email:", value=split[0], inline=False)
         embed.add_field(name="Password:", value=split[2], inline=False)
         await bot.send_message(ctx.message.author, embed=embed)
 
-        print("{} Typed <minecraft".format(userName))
-        
+        print("{} Typed <minecraft.format(userName))
+
+        client = discord.Client()
 bot.run("NDcxOTQ3NjQxODgyODA0MjI0.DjsRWw.xzK_Vc3MNSDBpiceFqt_7sNauAk")
