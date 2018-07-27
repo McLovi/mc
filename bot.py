bf059d441bc592d3612697f1cc33c7172415902c
JBIsaik <41596157+JBIsaik@users.noreply.github.com>
2018-07-27 12:37:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ lines = open(r'usermcalts.txt').read().splitlines()
 @bot.event
 async def on_ready():
     print('The bot is online!')
-    await bot.change_presence(game=discord.Game(name='Type <spotify'))
+    await bot.change_presence(game=discord.Game(name='Type <minecraft'))
    
     
 @bot.command(pass_context=True)
@@ -22,7 +22,7 @@ async def minecraft(ctx):
         myline = random.choice(lines)
         split = myline.partition(":")
        
-        embed=discord.Embed(title="Spotify Account", color=0xf45eff)
+        embed=discord.Embed(title="Minecraft Account", color=0xf45eff)
         embed.set_thumbnail(url="https://pre00.deviantart.net/3d13/th/pre/i/2016/343/0/7/free_minecraft_logo_template_by_curtzeninja-dar2dou.png")
         embed.add_field(name="Email:", value=split[0], inline=False)
         embed.add_field(name="Password:", value=split[2], inline=False)
