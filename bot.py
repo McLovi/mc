771f04d1f794e2e05b258aa99a7684c1f93933a7
JBIsaik <41596157+JBIsaik@users.noreply.github.com>
2018-07-27 13:59:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ lines = open(r'usermcalts.txt').read().splitlines()
 @bot.event
 async def on_ready():
     print('The bot is online!')
-    await bot.change_presence(game=discord.Game(name='Type <getalt mc'))
+    await bot.change_presence(game=discord.Game(name='Type $getalt mc'))
    
     
 @bot.command(pass_context=True)
